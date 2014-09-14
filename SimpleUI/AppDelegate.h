1e4aca02ca4c6bea96e5d85080586303cc788fb7
zhangshiyu <ericgo@zhangshiyus-mbp.cable.rcn.com>
2014-09-13 22:52:23 -0400

    Initial Commit

---
+++ SimpleUI/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  SimpleUI
+//
+//  Created by zhangshiyu on 9/13/14.
+//  Copyright (c) 2014 ZSY. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
